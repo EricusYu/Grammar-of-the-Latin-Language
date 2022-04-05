1086be7489f2a7328abe8cdbb446134391625bdb
Eric Yu <leyan.yu@shp.org>
2022-04-05 14:15:20 -0400

    Initial Commit

---
+++ Declensions of the Latin Language/Declensions_of_the_Latin_LanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Declensions_of_the_Latin_LanguageApp.swift
+//  Declensions of the Latin Language
+//
+//  Created by Leyan Yu on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Declensions_of_the_Latin_LanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
